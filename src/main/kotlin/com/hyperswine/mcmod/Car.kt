0dfe2bfee7275ef33006adfff0ae562c66431993
Jason Qin <jiasheng.qin@student.unsw.edu.au>
2022-08-26 00:42:59 +1000

    No I dont

--- src/main/kotlin/com/hyperswine/mcmod/Car.kt
+++ src/main/kotlin/com/hyperswine/mcmod/Car.kt
@@ -3,6 +3,7 @@ package com.hyperswine.mcmod
 import net.fabricmc.api.ModInitializer
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricDefaultAttributeRegistry
 import net.fabricmc.fabric.api.`object`.builder.v1.entity.FabricEntityTypeBuilder
+import net.fabricmc.fabric.impl.registry.sync.FabricRegistry
 import net.minecraft.entity.Entity
 import net.minecraft.entity.EntityDimensions
 import net.minecraft.entity.EntityType
@@ -15,34 +16,24 @@ import net.minecraft.util.registry.Registry
 import net.minecraft.world.World
 
 class Car : ModInitializer {
-    // var car: CarEntity = CarEntity(TODO(), TODO())
-
-    // register c ar
-    final val car: EntityType<CarEntity> = Registry.register(
+    // type EntityType<CarEntity>
+    val car = Registry.register(
         Registry.ENTITY_TYPE, Identifier("Car", "CarEntity"),
-        FabricEntityTypeBuilder.create<CarEntity>(SpawnGroup.MISC, CarEntity)
+        FabricEntityTypeBuilder.create<CarEntity>(SpawnGroup.MISC, ::CarEntity)
             .dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
     )
 
-    val caraware = Registry.register(
-        Registry.ENTITY_TYPE,
-        Identifier("cartesting", "car"),
-        FabricEntityTypeBuilder.create<CarAware>(SpawnGroup.CREATURE, CarAware::new)
-            .dimensions(EntityDimensions.fixed(0.75f, 0.75f)).build()
-    );
-
     override fun onInitialize() {
-        FabricDefaultAttributeRegistry.register(null, null)
+        //  FabricDefaultAttributeRegistry.register(car, CarEntity::createSpawnPacket)
+
     }
 }
-
-class CarAware(entityType: EntityType<out PathAwareEntity>?, world: World?) : PathAwareEntity(entityType, world)
 
 /// So you construct this "directly" instead of SECONDARY
 class CarEntity(type: EntityType<*>?, world: World?) : Entity(type, world) {
     // attributes, like accel, curr_velocity?
     // acceleration depends on how much the dude is stepping on the gas
-    // there is a max velocity though, so even while your accelerating and your v = max, you wont
+    // there is a max velocity though, so even while your accelerating and your v = max, you won't
     // gain anymore speed
 
     var w_pressed: Boolean = false
@@ -69,9 +60,4 @@ class CarEntity(type: EntityType<*>?, world: World?) : Entity(type, world) {
         TODO("Not yet implemented")
     }
 
-    companion object CarEntity : EntityType.EntityFactory<Entity> {
-        override fun create(type: EntityType<Entity>?, world: World?): CarEntity? {
-            return null
-        }
-    }
 }
